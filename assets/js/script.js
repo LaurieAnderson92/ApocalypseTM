4a02b930a76d31b0f3aac39b12ba931ab8cc0b4e
Laurie Anderson <laurieanderson92@hotmail.co.uk>
2024-03-15 12:10:01 +0000

    script: Added a function to display event objects to DOM.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 
-document.addEventListener("DOMContentLoaded", function(){
-let eventInfo = [
+/* This creats the event objects which are used on every page, They are designed to be updated
+once a year with each event for that year being it's own object.
+*/
+let events = [
     // Below is the details for event 1
     {eventName: "London Has Fallen",
     eventDate: "June 18th",
@@ -17,17 +19,25 @@ let eventInfo = [
     eventLocation: "TBD",
     eventInfo: "Placeholder for description of Event 3"}
     ];
-let buttons = document.getElementsByClassName("header-btn");
-    for (let button of buttons){
-        button.addEventListener("click", function(eventInfo){
-            if (this.value == "Event"+(button+1)){
-                document.getElementById("header-event-infomation").innerHTML = 
-                `<h3>${eventName}</h3> <br>
-                Date: ${eventDate} Location: ${eventLocation} <br>
-                <p>${eventInfo}</p>
-                `
-            }
-        })
+
+//Code that runs on a click on the DOM
+
+
+//
+
+
+// Code that runs when the DOM loads
+
+//This function gets the event objects and displays each event as a individual div
+document.addEventListener("DOMContentLoaded", function displayEvents(){
+    console.log("function display Events started")
+    for (const event in events) {
+        console.log("loading:" + event)
+        console.log("loading:" + events[event].eventName)
+        document.getElementById('header-event-infomation').innerHTML += `<div id="Event:${event}"> 
+        ${events[event].eventDate} - ${events[event].eventName}<br>
+        ${events[event].eventLocation}
+         </div>`
+        }
     }
-    console.log("Completed the function")
-})
+)
